3ccf8c1fcd93a1321987bbbcced2f18379795cb4
Srivalli <81727998+CHAVALI26@users.noreply.github.com>
2025-12-16 01:21:56 +0530

    Implement data validation and preprocessing pipeline

---
+++ src/train.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from src.data_validation import DataValidator
+from src.preprocessing import DataPreprocessor
+
+def main():
+  raw_path = "data/raw/hr_attrition_raw.csv"
+  df = pd.read_csv(raw_path)
+  """Validation"""
+  validator = DataValidator()
+  validator.run_all_checks(df)
+  validated_path = "data/validated/hr_attrition_validated.csv"
+  df.to_csv(validated_path, index=False)
+  print("validated data saved successfully")
+  """Preprocessing"""
+  df= pd.read_csv("data/validated/hr_attrition_validated.csv")
+  preprocessor = DataPreprocessor()
+  processed_df = preprocessor.preprocess(df)
+  processed_df.to_csv("data/processed/hr_attrition_processed.csv", index=False)
+  Print("Data preprocessing completed")
+
+if __name__ == "__main__":
+  main()
+  
